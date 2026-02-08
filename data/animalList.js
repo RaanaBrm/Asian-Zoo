dbbd477b03ee0533306f39bff0b9cfd91096b04c
Maho Mikami <maho.cx1029@gmail.com>
2026-02-08 17:39:36 +0100

    add japanese animal info to object, and add images

--- data/animalList.js
+++ data/animalList.js
@@ -1,1 +1,43 @@
 // animal object
+export const JapaneseAnimalList = [
+  {
+    name: "Tanuki (Raccoon Dog)",
+    type: "Mammal",
+    size: "68 cm",
+    weight: "6 kg",
+    food: "Fruits, insects, small animals",
+    description:
+      "The tanuki is a nocturnal mammal native to Japan. It is highly adaptable and can live in forests, rural areas, and even near cities. Tanuki are well known in Japanese folklore and are often portrayed as playful and mischievous creatures.",
+    image: "tanuki.jpg",
+  },
+  {
+    name: "Red-crowned crane",
+    type: "Bird",
+    size: "150 cm",
+    weight: "10 kg",
+    food: "Fish, insects, plants",
+    description:
+      "The red-crowned crane is one of the largest birds in Japan. It is famous for its graceful appearance and beautiful courtship dances. In Japanese culture, the crane symbolizes longevity, happiness, and good fortune.",
+    image: "red-crowned-crane.jpg",
+  },
+  {
+    name: "Japanese monkey",
+    type: "Mammal",
+    size: "60 cm",
+    weight: "11 kg",
+    food: "Fruits, leaves, insects",
+    description:
+      "The Japanese monkey, also known as the snow monkey, lives in cold mountainous regions of Japan. It is especially known for bathing in natural hot springs during winter. These monkeys are highly social and live in large groups.",
+    image: "japanese-monkey.jpg",
+  },
+  {
+    name: "Japanese black bear",
+    type: "Mammal",
+    size: "140 cm",
+    weight: "120 kg",
+    food: "Fruits, nuts, insects, small animals",
+    description:
+      "The Japanese black bear lives mainly in forested mountain areas. It is generally shy and avoids humans, but it can be dangerous if threatened. The bear plays an important role in the ecosystem by spreading seeds through its diet.",
+    image: "japanese-black-bear.jpg",
+  },
+];
