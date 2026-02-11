81717fc3a4e079fd2ca8bfd09e11eb1298e27384
Maho Mikami <maho.cx1029@gmail.com>
2026-02-11 19:31:41 +0100

    merged all animal info to one list, add chinese animals images

--- data/animalList.js
+++ data/animalList.js
@@ -1,6 +1,7 @@
 // animal object
-export const JapaneseAnimalList = [
+export const animalList = [
   {
+    id: "tanuki",
     name: "Tanuki (Raccoon Dog)",
     type: "Mammal",
     size: "68 cm",
@@ -11,16 +12,7 @@ export const JapaneseAnimalList = [
     image: "tanuki.jpg",
   },
   {
-    name: "Red-crowned crane",
-    type: "Bird",
-    size: "150 cm",
-    weight: "10 kg",
-    food: "Fish, insects, plants",
-    description:
-      "The red-crowned crane is one of the largest birds in Japan. It is famous for its graceful appearance and beautiful courtship dances. In Japanese culture, the crane symbolizes longevity, happiness, and good fortune.",
-    image: "red-crowned-crane.jpg",
-  },
-  {
+    id: "japanese-monkey",
     name: "Japanese monkey",
     type: "Mammal",
     size: "60 cm",
@@ -31,6 +23,7 @@ export const JapaneseAnimalList = [
     image: "japanese-monkey.jpg",
   },
   {
+    id: "japanese-black-bear",
     name: "Japanese black bear",
     type: "Mammal",
     size: "140 cm",
@@ -39,5 +32,77 @@ export const JapaneseAnimalList = [
     description:
       "The Japanese black bear lives mainly in forested mountain areas. It is generally shy and avoids humans, but it can be dangerous if threatened. The bear plays an important role in the ecosystem by spreading seeds through its diet.",
     image: "japanese-black-bear.jpg",
+  },
+  {
+    id: "hoopoe",
+    name: "Hoopoe (Hoodhood)",
+    type: "Bird",
+    country: "iran",
+    size: "28 cm",
+    weight: "0.09 kg",
+    food: "Insects, larvae",
+    description:
+      "The hoopoe is a distinctive bird native to Iran and many parts of the Middle East.",
+    image: "hodhood.png",
+  },
+  {
+    id: "persian-partridge",
+    name: "Persian Partridge",
+    type: "Bird",
+    country: "iran",
+    size: "35 cm",
+    weight: "0.6 kg",
+    food: "Seeds, grains, insects",
+    description:
+      "The Persian partridge is commonly found in mountainous regions of Iran.",
+    image: "partridge.jpeg",
+  },
+  {
+    id: "peregrine-falcon",
+    name: "Peregrine Falcon",
+    type: "Bird",
+    country: "iran",
+    size: "45 cm",
+    weight: "1 kg",
+    food: "Small birds",
+    description:
+      "The peregrine falcon is one of the fastest birds in the world.",
+    image: "peregrine-falcon.png",
+  },
+  {
+    id: "giant-panda",
+    name: "Giant Panda",
+    type: "Mammal",
+    country: "china",
+    size: "1.2 - 1.9 m",
+    weight: "85 - 125 kg",
+    food: "Bamboo",
+    description:
+      "The giant panda is a bear species endemic to China. It is characterized by its bold black-and-white coat and round body. It spends most of its day eating bamboo in the mountainous forests of Sichuan, Shaanxi, and Gansu provinces.",
+    image: "giant-panda.jpg",
+  },
+  {
+    id: "golden-snub-nosed-monkey",
+    name: "Golden Snub-nosed Monkey",
+    type: "Mammal",
+    country: "china",
+    size: "50 - 70 cm",
+    weight: "12 - 15 kg",
+    food: "Leaves, fruits, bark, and seeds",
+    description:
+      "This highly social monkey lives in the mountainous forests of central and southwestern China. It is recognized by its bright golden fur and distinctive flat, snub nose. These features help it survive in cold and snowy environments.",
+    image: "golden-snub-nosed-monkey.jpg",
+  },
+  {
+    id: "red-panda",
+    name: "Red Panda",
+    type: "Mammal",
+    country: "china",
+    size: "50 - 64 cm",
+    weight: "3 - 6 kg",
+    food: "Bamboo, berries, blossoms, and small insects",
+    description:
+      "The red panda is a small arboreal mammal native to the eastern Himalayas and southwestern China. It is known for its reddish-brown fur, white facial markings, and long bushy tail, which it uses to keep warm in cold mountain climates.",
+    image: "red-panda.jpg",
   },
 ];
