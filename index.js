077629fd44fd19990e86cf0121963b202d43b4d2
Maho Mikami <maho.cx1029@gmail.com>
2026-02-11 21:40:45 +0100

    merge japanese router to develop

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import AnimalRoute from "./routes/AnimalRoute.js";
 import express from "express";
 import * as path from "path";
 import "dotenv/config";
+import japaneseAnimalRouter from "./routes/JapaneseAnimalRoute";
 
 import PersianAnimalRoute from "./routes/PersianAnimalRoute.js";
 import AnimalRoute from "./routes/AnimalRoute.js";
@@ -18,14 +19,14 @@ app.use(express.static("public"));
 app.use("/", PersianAnimalRoute);
 app.use("/", AnimalRoute);
 
-
-
-app.use("/", PersianAnimalRoute);
-app.use("/", AnimalRoute);
-
 app.get("/", (req, res) => {
-  res.redirect("/persian-animals");
+  res.render("pages/index", {
+    headTitle: "Asian Zoo | Home",
+    welcomeMessage: "Welcome to our special exhibition of Asian wildlife!",
+  });
 });
+
+app.use("/japanese-animals", japaneseAnimalRouter);
 
 app.listen(port, () => {
   console.log(`I'm listening port ${port}`);
