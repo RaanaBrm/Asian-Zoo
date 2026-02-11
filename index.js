1048c77c664b3d06a35dc779ad58f27833dfe4f9
Maho Mikami <maho.cx1029@gmail.com>
2026-02-11 22:50:06 +0100

    fixed some error of main

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import PersianAnimalRoute from "./routes/PersianAnimalRoute.js";
-import AnimalRoute from "./routes/AnimalRoute.js";
 import express from "express";
 import * as path from "path";
 import "dotenv/config";
-import japaneseAnimalRouter from "./routes/JapaneseAnimalRoute";
-
-import PersianAnimalRoute from "./routes/PersianAnimalRoute.js";
-import AnimalRoute from "./routes/AnimalRoute.js";
+import japaneseAnimalRouter from "./routes/JapaneseAnimalRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3040;
@@ -16,13 +11,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
-app.use("/", PersianAnimalRoute);
-app.use("/", AnimalRoute);
 
 app.get("/", (req, res) => {
   res.render("pages/index", {
     headTitle: "Asian Zoo | Home",
     welcomeMessage: "Welcome to our special exhibition of Asian wildlife!",
+    selectedAnimal: null,
   });
 });
 
--- routes/JapaneseAnimalRoute.js
+++ routes/JapaneseAnimalRoute.js
@@ -1,20 +1,18 @@
 import express from "express";
 import * as path from "path";
-import { japaneseAnimalList } from "../data/animalList.js";
+import { animalList } from "../data/animalList.js";
 
 const japaneseAnimalRouter = express.Router();
 const __dirname = path.resolve();
 
-japaneseAnimalRputer.get("/", (req, res) => {
-  const animal = japaneseAnimalList.find(
-    (animalList) => animalList.id === req.query.id
-  );
+japaneseAnimalRouter.get("/", (req, res) => {
+  const animal = animalList.find((animal) => animal.id === req.query.id);
   console.log(animal);
 
   res.render(path.join(__dirname, "/views/pages/animal.ejs"), {
     headTitle: "Japanese Animals",
     pageType: "japan",
-    animalList: japaneseAnimalList,
+    animalList: animalList,
     selectedAnimal: animal,
   });
 });
