d9ca653ed6e26cc313c44e0865f755062c6c76c2
Maho Mikami <maho.cx1029@gmail.com>
2026-02-12 13:46:23 +0100

    add chinese router and create sidebar for index and each animal pages

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import * as path from "path";
 import "dotenv/config";
 import japaneseAnimalRouter from "./routes/JapaneseAnimalRoute.js";
+import persianAnimalRouter from "./routes/PersianAnimalRoute.js";
+import chineseAnimalRouter from "./routes/ChineseAnimalRouter";
+import { animalList } from "./data/animalList.js";
 
 const app = express();
 const port = process.env.PORT || 3040;
@@ -13,14 +16,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  const animal = animalList.find((animal) => animal.id === req.query.id);
+  console.log(animal);
   res.render("pages/index", {
     headTitle: "Asian Zoo | Home",
     welcomeMessage: "Welcome to our special exhibition of Asian wildlife!",
-    selectedAnimal: null,
+    pageType: "index",
+    animalList: animalList,
+    selectedAnimal: animal,
   });
 });
 
 app.use("/japanese-animals", japaneseAnimalRouter);
+app.use("/persian-animals", persianAnimalRouter);
+app.use("/chinese-animals", chineseAnimalRouter);
 
 app.listen(port, () => {
   console.log(`I'm listening port ${port}`);
